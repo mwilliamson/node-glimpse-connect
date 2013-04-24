ef78c1a0b4c5cf4da0d381536f37c3a6a1bee14f
Michael Williamson <mike@zwobble.org>
2013-04-24 15:56:22 +0100

    Generate uri for request in glimpse data

--- tests/request-data.tests.js
+++ tests/request-data.tests.js
@@ -4,10 +4,16 @@ var DataStore = require("../lib/request-data").DataStore;
 var requestId = "(request id)";
 
 exports["basic glimpse data is extracted from request"] = function(test) {
-    var request = fakeRequest({method: "GET"});
+    var request = fakeRequest({
+        method: "GET",
+        headers: {'host': 'eg.com'},
+        url: "/hello"
+    });
     var glimpseData = glimpseDataForRequest(request)
     test.equal(glimpseData.method, "GET");
     test.equal(glimpseData.requestId, requestId);
+    // TODO: https
+    test.equal(glimpseData.uri, "http://eg.com/hello");
     test.ok(glimpseData.data);
     test.done();
 };
