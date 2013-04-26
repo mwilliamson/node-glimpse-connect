cd65ca6021d08c3ba7d6384503b70aee1da4407b
Michael Williamson <mike@zwobble.org>
2013-04-26 09:51:19 +0100

    Add sensible implementation of text/html content-type detection

--- tests/content-types.tests.js
+++ tests/content-types.tests.js
@@ -3,9 +3,21 @@ var contentTypes = require("../lib/content-types");
 
 exports["text/html is detected as HTML content type"] =
     isHtmlTestCase("text/html", true);
+    
+exports["TEXT/HTML is detected as HTML content type"] =
+    isHtmlTestCase("TEXT/HTML", true);
 
 exports["text/plain is not detected as HTML content type"] =
     isHtmlTestCase("text/plain", false);
+
+exports["text/html with encoding is detected as HTML content type"] =
+    isHtmlTestCase("text/html; charset=ISO-8859-4", true);
+    
+exports["ztext/html is not detected as HTML content type"] =
+    isHtmlTestCase("ztext/html", false);
+    
+exports["text/htmlz is not detected as HTML content type"] =
+    isHtmlTestCase("text/htmlz", false);
 
 function isHtmlTestCase(contentType, expectedIsHtml) {
     return function(test) {
